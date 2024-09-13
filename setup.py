04410774d465c2bd4ca26d9c806b1c05bd3a04af
Niarea <niareadama190796@gmail.com>
2024-09-13 01:41:23 +0100

    dvc added

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Chicken-Disease-Classification--Project"
+REPO_NAME = "Chicken-Disease-Classification-Project"
 AUTHOR_USER_NAME = "Niarea"
 SRC_REPO = "cnnClassifier"
 AUTHOR_EMAIL = "niareadama190796@gmail.com"
